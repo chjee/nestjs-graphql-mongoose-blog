920877bb4307a0f3f97495a4a3198de26d82e10e
Changhoon-Jee <chjee71@gmail.com>
2023-12-11 19:33:58 +0900

    add class-validators.

--- src/categories/dto/create-category.input.ts
+++ src/categories/dto/create-category.input.ts
@@ -1,7 +1,11 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 @InputType()
 export class CreateCategoryInput {
   @Field(() => String, { description: 'Category name' })
+  @IsNotEmpty()
+  @IsString()
+  @Length(4, 60)
   name!: string;
 }
