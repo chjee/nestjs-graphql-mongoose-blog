612607091d7de4d612d81efdda0087ad2054b151
Changhoon-Jee <chjee71@gmail.com>
2023-12-13 18:59:13 +0900

    add a users service.

--- src/profiles/profiles.module.ts
+++ src/profiles/profiles.module.ts
@@ -3,11 +3,16 @@ import { ProfilesService } from './profiles.service';
 import { ProfilesResolver } from './profiles.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Profile, ProfileSchema } from './entities/profile.entity';
+import { User, UserSchema } from './../users/entities/user.entity';
+import { UsersService } from './../users/users.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Profile.name, schema: ProfileSchema }]),
+    MongooseModule.forFeature([
+      { name: Profile.name, schema: ProfileSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
-  providers: [ProfilesResolver, ProfilesService],
+  providers: [ProfilesResolver, ProfilesService, UsersService],
 })
 export class ProfilesModule {}
