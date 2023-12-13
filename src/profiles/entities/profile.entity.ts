5c8a9ade48495909014127e0bb8930db1bd7533c
Changhoon-Jee <chjee71@gmail.com>
2023-12-13 18:58:46 +0900

    add some props & fields.

--- src/profiles/entities/profile.entity.ts
+++ src/profiles/entities/profile.entity.ts
@@ -1,6 +1,7 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Schema as MongooseSchema, HydratedDocument } from 'mongoose';
+import { User } from './../../users/entities/user.entity';
 
 export type ProfileDocument = HydratedDocument<Profile>;
 
@@ -11,13 +12,27 @@ export class Profile {
   @Field(() => String, { description: 'User id' })
   _id!: string;
 
+  @Field(() => String, { description: 'Profile slug' })
+  id!: string;
+
   @Prop({ type: String, required: true, trim: true })
   @Field(() => String, { description: 'User Bio' })
   bio!: string;
 
-  @Prop({ type: [{ type: MongooseSchema.Types.ObjectId, ref: 'User' }] })
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'User' })
   @Field(() => String, { description: 'User ID' })
   userId!: string;
+
+  @Prop({ type: Date, default: Date.now })
+  @Field(() => Date, { nullable: true, description: 'Profile created date' })
+  createdAt?: Date;
+
+  @Prop({ type: Date, default: Date.now })
+  @Field(() => Date, { nullable: true, description: 'Profile updated date' })
+  updatedAt?: Date;
+
+  @Field(() => User, { nullable: true, description: 'User' })
+  user?: User;
 }
 
 export const ProfileSchema = SchemaFactory.createForClass(Profile);
