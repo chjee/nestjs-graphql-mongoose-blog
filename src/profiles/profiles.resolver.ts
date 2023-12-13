dca68959d40a014e0c4e940bfe1ce36647a2024a
Changhoon-Jee <chjee71@gmail.com>
2023-12-13 19:02:12 +0900

    add a users service for resolve field & modify functions.

--- src/profiles/profiles.resolver.ts
+++ src/profiles/profiles.resolver.ts
@@ -1,12 +1,26 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+  ID,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { ProfilesService } from './profiles.service';
 import { Profile } from './entities/profile.entity';
 import { CreateProfileInput } from './dto/create-profile.input';
 import { UpdateProfileInput } from './dto/update-profile.input';
+import { UsersService } from './../users/users.service';
+import { User } from './../users/entities/user.entity';
 
 @Resolver(() => Profile)
 export class ProfilesResolver {
-  constructor(private readonly profilesService: ProfilesService) {}
+  constructor(
+    private readonly profilesService: ProfilesService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Mutation(() => Profile)
   async createProfile(
@@ -19,7 +33,7 @@ export class ProfilesResolver {
   async findAll(
     @Args('skip', { type: () => Int, nullable: true }) skip?: number,
     @Args('limit', { type: () => Int, nullable: true }) limit?: number,
-  ): Promise<Profile[]> {
+  ): Promise<Profile[] | null> {
     return this.profilesService.findAll({
       skip: skip,
       limit: limit,
@@ -28,23 +42,31 @@ export class ProfilesResolver {
 
   @Query(() => Profile, { name: 'getProfileById' })
   async findOne(
-    @Args('id', { type: () => String }) id: string,
-  ): Promise<Profile> {
-    return this.profilesService.findOne(id);
+    @Args('id', { type: () => ID }) id: string,
+  ): Promise<Profile | null> {
+    return this.profilesService.findOne({ _id: id });
+  }
+
+  @ResolveField(() => Profile)
+  async user(@Parent() { userId }: Profile): Promise<User | null> {
+    return this.usersService.findOne({ _id: userId });
   }
 
   @Mutation(() => Profile)
   async updateProfile(
-    @Args('id', { type: () => String }) id: string,
+    @Args('id', { type: () => ID }) id: string,
     @Args('updateProfileInput') updateProfileInput: UpdateProfileInput,
   ): Promise<any> {
-    return this.profilesService.update(id, updateProfileInput);
+    return this.profilesService.update({
+      where: { _id: id },
+      data: updateProfileInput,
+    });
   }
 
   @Mutation(() => Profile)
   async removeProfile(
-    @Args('id', { type: () => String }) id: string,
+    @Args('id', { type: () => ID }) id: string,
   ): Promise<any> {
-    return this.profilesService.remove(id);
+    return this.profilesService.remove({ _id: id });
   }
 }
