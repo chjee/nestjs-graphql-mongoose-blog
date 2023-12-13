e0184ab1d7d97a447ac1a770a65caaa8cacb0a69
Changhoon-Jee <chjee71@gmail.com>
2023-12-13 19:56:23 +0900

    add a user & profile service for resolve field & modify functions.

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,12 +1,29 @@
-import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+  ID,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { PostsService } from './../posts/posts.service';
+import { Post } from 'src/posts/entities/post.entity';
+import { ProfilesService } from './../profiles/profiles.service';
+import { Profile } from './../profiles/entities/profile.entity';
 
 @Resolver(() => User)
 export class UsersResolver {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly postsService: PostsService,
+    private readonly profilesService: ProfilesService,
+  ) {}
 
   @Mutation(() => User)
   async createUser(
@@ -19,7 +36,7 @@ export class UsersResolver {
   async findAll(
     @Args('skip', { type: () => Int, nullable: true }) skip?: number,
     @Args('limit', { type: () => Int, nullable: true }) limit?: number,
-  ): Promise<User[]> {
+  ): Promise<User[] | null> {
     return this.usersService.findAll({
       skip: skip,
       limit: limit,
@@ -27,8 +44,22 @@ export class UsersResolver {
   }
 
   @Query(() => User, { name: 'getUserById' })
-  async findOne(@Args('id', { type: () => ID }) id: string): Promise<User> {
-    return this.usersService.findOne(id);
+  async findOne(
+    @Args('id', { type: () => ID }) id: string,
+  ): Promise<User | null> {
+    return this.usersService.findOne({ _id: id });
+  }
+
+  @ResolveField(() => User)
+  async posts(@Parent() { id }: User): Promise<Post[] | null> {
+    return this.postsService.findAll({
+      where: { userId: id },
+    });
+  }
+
+  @ResolveField(() => User)
+  async profile(@Parent() { id }: User): Promise<Profile | null> {
+    return this.profilesService.findOne({ userId: id });
   }
 
   @Mutation(() => User)
@@ -36,13 +67,14 @@ export class UsersResolver {
     @Args('id', { type: () => ID }) id: string,
     @Args('updateUserInput') updateUserInput: UpdateUserInput,
   ): Promise<any> {
-    return this.usersService.update(id, updateUserInput);
+    return this.usersService.update({
+      where: { _id: id },
+      data: updateUserInput,
+    });
   }
 
   @Mutation(() => User)
-  async removeUser(
-    @Args('id', { type: () => String }) id: string,
-  ): Promise<any> {
-    return this.usersService.remove(id);
+  async removeUser(@Args('id', { type: () => ID }) id: string): Promise<any> {
+    return this.usersService.remove({ _id: id });
   }
 }
