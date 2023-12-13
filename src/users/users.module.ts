e8b40cf93fe172acdd9a883aaf6c01182d084dad
Changhoon-Jee <chjee71@gmail.com>
2023-12-13 19:55:10 +0900

    add post & profile services.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { User, UserSchema } from './entities/user.entity';
+import { Post, PostSchema } from './../posts/entities/post.entity';
+import { PostsService } from './../posts/posts.service';
+import { Profile, ProfileSchema } from './../profiles/entities/profile.entity';
+import { ProfilesService } from './../profiles/profiles.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Post.name, schema: PostSchema },
+      { name: Profile.name, schema: ProfileSchema },
+    ]),
   ],
-  providers: [UsersResolver, UsersService],
+  providers: [UsersResolver, UsersService, PostsService, ProfilesService],
 })
 export class UsersModule {}
